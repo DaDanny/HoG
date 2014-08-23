a657b5193430e2d41bbaeef2b9d3b2de3742b1d7
Stephen Cheung <stephencheung@zkron.com>
2014-08-23 12:42:28 -0700

    working carousel with custom images

---
+++ app/scripts/controllers/CarouselCtrl.js
@@ -0,0 +1,34 @@
+function CarouselCtrl($scope) {
+  $scope.myInterval = -1;
+  var slides = $scope.slides = [];
+
+  // image array pulled from MongoDB
+  var imageArray = [];
+
+	// replace this with FB integration
+	var obj_1 = {
+		image: 'images/feature_person.jpg',
+		text: '"Gastown is awesome"'
+	};
+	
+	var obj_2 = {
+		image: 'images/gastown0.jpg',
+		text: '"Gastown 2 is awesome"'
+	};
+	
+	imageArray.push(obj_1);
+	imageArray.push(obj_2);
+	
+	
+
+  $scope.addImagesToCarousel = function() {
+	var imageArraySize = imageArray.length;
+	
+	for(var i=0; i < imageArraySize; i++)
+	{
+		slides.push(imageArray[i]);
+	}
+  };
+
+	$scope.addImagesToCarousel();
+}
