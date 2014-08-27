83d9e580d33a71b939517fa2fc2256cbd674edc7
DaDanny <DanFrancken@gmail.com>
2014-08-27 11:04:29 -0700

    Fixed copy folders

--- app copy/scripts/controllers/about.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('hoGApp')
-  .controller('AboutCtrl', function ($scope, $http) {
-    
-  });
--- app copy/scripts/services/personservice.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('hoGApp')
-  .service('Personservice', function($http,$q) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
